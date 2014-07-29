f61bcfa1c605eb33fde8dea3a08b602a1c5c69da
Ricardo Lazaro de Oliveira <lazaro@99taxis.com>
2014-07-29 19:59:46 -0300

    list s3 files

--- src/main/scala/com/taxis99/aws/S3Helper.scala
+++ src/main/scala/com/taxis99/aws/S3Helper.scala
@@ -1,18 +1,26 @@
 package com.taxis99.aws
 
+import scala.collection.JavaConversions._
+
 import com.amazonaws.{ Protocol, ClientConfiguration }
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 
 trait S3Helper {
 
+  val awsCredential = Configuration.awsCredentials.accessKey
+
+  val awsSecret = Configuration.awsCredentials.secretKey
+
   lazy val s3 =  {
     val clientConfig = new ClientConfiguration()
     clientConfig.setProtocol(Protocol.HTTP)
+    new AmazonS3Client(new BasicAWSCredentials(awsCredential, awsSecret), clientConfig)
+  }
 
-    new AmazonS3Client(
-      new BasicAWSCredentials(Configuration.awsCredentials.accessKey, Configuration.awsCredentials.secretKey),
-      clientConfig)
+  def listFiles(bucketName: String, prefix: String) = {
+    s3.listObjects(bucketName, prefix).getObjectSummaries.sortBy(_.getLastModified).reverse
   }
 
 }
+
