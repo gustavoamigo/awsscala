d35c1b5c3b8ce097ba78d85d7963b98078779a2f
Miguel Mendes Ruiz <migmruiz@gmail.com>
2014-09-17 15:23:48 -0300

    receiving multiple messages from SQS

--- src/main/scala/com/taxis99/aws/SQSHelper.scala
+++ src/main/scala/com/taxis99/aws/SQSHelper.scala
@@ -8,28 +8,39 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.sqs.model._
 
 /**
- * Helper to handle SQS Interface ( lots of uggly Java Api )
+ * Helper to handle SQS Interface
  */
 class SQSHelper(accessKey: String, secretKey: String, queueName: String, endpoint: String) {
   
-  private val client = new AmazonSQSAsyncClient(new BasicAWSCredentials(accessKey, secretKey))
-  client.setEndpoint(endpoint)
-
-  private lazy val queueUrl = client.createQueue(new CreateQueueRequest(queueName)).getQueueUrl
-
-  def fetchMessage = {
-    val request = new ReceiveMessageRequest(queueUrl).withMaxNumberOfMessages(1).withAttributeNames("ApproximateReceiveCount", "SentTimestamp")
-    client.receiveMessage(request).getMessages().asScala.toList.headOption
+  private lazy val (client, queueUrl) = {
+    val newClient = new AmazonSQSClient(new BasicAWSCredentials(accessKey, secretKey))
+    newClient.setEndpoint(endpoint)
+    val newQueueUrl = newClient.createQueue(new CreateQueueRequest(queueName)).getQueueUrl
+    (newClient, newQueueUrl)
   }
 
-  def deleteMessage(message: Message) = {
+  def fetchMessages(maxNumberOfMessages: Int = 1): List[Message] = {
+    val request = (new ReceiveMessageRequest(queueUrl)).withMaxNumberOfMessages(maxNumberOfMessages).withAttributeNames("ApproximateReceiveCount", "SentTimestamp")
+    client.receiveMessage(request).getMessages().asScala.toList
+  }
+
+  def fetchApproximatlyAllMessages(numberOfMessages: Int = 10): List[Message] = {
+    fetchMessages(approximateNumberOfMessages + numberOfMessages)
+  }
+
+  def deleteMessage(message: Message): Unit = {
     if (message != null) {
       client.deleteMessage(new DeleteMessageRequest(queueUrl, message.getReceiptHandle))
     }
   }
 
-  def send(body: String) = {
+  def send(body: String): Unit = {
     client.sendMessage(new SendMessageRequest(queueUrl, body))
+  }
+
+  def approximateNumberOfMessages(): Integer = {
+    client.getQueueAttributes(new GetQueueAttributesRequest(queueUrl, List("ApproximateNumberOfMessages").asJava))
+      .getAttributes.get("ApproximateNumberOfMessages").toInt
   }
 
 }
